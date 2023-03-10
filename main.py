8fe1e943df620b37219bebf6e89529377bdeb279
Romanin <60302782+romanin-rf@users.noreply.github.com>
2023-03-10 15:22:36 +0200

    Забыл залить

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ from kivy.lang import Builder
 from kivymd.uix.boxlayout import MDBoxLayout
 
 # ! Main
-class ContentNavigationDrawer(MDBoxLayout):
-    pass
-
-class MainApp(MDApp):
+class MeSureMVPApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Blue"
         return Builder.load_file("main.kv")
 
-app = MainApp()
-
+app = MeSureMVPApp()
 app.run()
