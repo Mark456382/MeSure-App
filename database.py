84a7fb2ccf39b110c1be16d85e950ecd691038d5
Mark3306 <94844045+Mark456382@users.noreply.github.com>
2023-03-11 13:48:15 +0200

    add key

--- database.py
+++ database.py
@@ -3,3 +3,4 @@ from firebase_admin import credentials
 
 cred = credentials.Certificate("/home/mark/Документы/GitHub/ReactNodeApp/mesure-48110-firebase-adminsdk-mz68v-72e3e719ed.json")
 firebase_admin.initialize_app(cred)
+firebase_admin.reference("/users_database/").set("__info__.json")
