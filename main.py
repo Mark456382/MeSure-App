cd2c6b87a2b3638159fd0b58c48ea3f93c346b36
Romanin <60302782+romanin-rf@users.noreply.github.com>
2023-03-11 20:31:55 +0200

    Архитектура
    Ну типа)

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import sys
 from kivymd.app import MDApp
-from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
+from kivy.lang import Builder
 # > Typing
 from typing import Dict, Any
 # > Rich
 from rich.console import Console
+# > Local Import
+from settings import DEBUG, THEME_STYLE, PRIMARY_PALETTE, MAIN_UI_FILE
 
 # > Init
 console = Console()
-DEBUG = True
 
 # > Functions
 def lsattr(obj: object) -> Dict[str, Any]:
@@ -22,12 +22,12 @@ def lsattr(obj: object) -> Dict[str, Any]:
 # > Main
 class MeSureMVPApp(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.theme_style = THEME_STYLE
+        self.theme_cls.primary_palette = PRIMARY_PALETTE
         
-        root: MDScreen = Builder.load_file("main.kv")
-        
-        if DEBUG:
+        root: MDScreen = Builder.load_file(MAIN_UI_FILE)
+
+        if DEBUG: 
             console.print(root.ids) # Если у объекта в файле main.kv прописан id, то они будут в root.ids
 
         return root
@@ -37,5 +37,6 @@ class MeSureMVPApp(MDApp):
             self.fps_monitor_start()
 
 # > Start
-app = MeSureMVPApp()
-app.run()
+if __name__ == "__main__":
+    app = MeSureMVPApp()
+    app.run()
---
+++ settings.py
@@ -0,0 +1,7 @@
+# > System
+MAIN_UI_FILE = "ui.kv"
+DEBUG = True
+
+# > Grahics
+THEME_STYLE = "Dark"
+PRIMARY_PALETTE = "Blue"
